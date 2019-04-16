509c6233cbeebb6af8dd790eaed6fa76897705e5
nitrag <nitrag@users.noreply.github.com>
2019-04-16 01:29:49 -0400

    Add customizable formatter
    Allow for customizing the format of the error messages:
    
    ```python
    import logging
    import stacklogging
    
    
    formatter = logging.Formatter("[%(levelname)s] [%(name)s:%(lineno)s - %(funcName)s() ] %(message)s")
    logger = stacklogging.getLogger(name, level=logging.DEBUG, formatter=formatter)
    ```

--- stacklogging/__init__.py
+++ stacklogging/__init__.py
@@ -2,12 +2,15 @@ from stacklogging.main import StackloggingHandler
 import logging
 
 
-def getLogger(name=None, level=logging.DEBUG):
+def getLogger(name=None, level=logging.DEBUG, formatter=None):
     logger = logging.getLogger(name)
     logger.setLevel(level)
 
     logger_handler = StackloggingHandler()
     logger_handler.setLevel(level)
+    if formatter:
+        logger_handler.setFormatter(formatter)
+
     logger.addHandler(logger_handler)
 
     return logger
