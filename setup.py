3ce71ccf60716ab9aa52ede1aa64cb02e73b60e2
David Gasquez <davidgasquez@gmail.com>
2019-04-16 09:03:04 -0700

    Add custom logger

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name="stacklogging",
     packages=find_packages(),
-    version="0.2.0",
+    version="0.2.1",
     description="Python structured logging with Google Cloud Stackdriver API integration",
     author="David Gasquez",
     license="MIT",
